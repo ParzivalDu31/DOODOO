ed146c4c54d58990629afcf826f5192f4ac66ea3
ParzivalDu31 <paulasarock@gmail.com>
2026-05-02 15:59:48 +0200

    Update DOODOO.py

--- DOODOO.py
+++ DOODOO.py
@@ -1,6 +1,5 @@
 from groq import Groq
 import streamlit as st
-from duckduckgo_search import DDGS
 st.title("Bonjour je suis doodoo l'ia de Paul")
 if "historique" not in st.session_state:
     st.session_state.historique = []
@@ -24,15 +23,13 @@ for msg in st.session_state.historique:
         st.write(msg["content"])
 message = st.chat_input("votre msg...")
 if message:
-    with DDGS() as ddgs:
-        resultats = list(ddgs.text(message, max_results=3))
-        contexte = "\n".join([r['body'] for r in resultats])
     st.session_state.historique.append({"role": "user", "content": message})
     
     reponse = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=[
-            {"role": "system", "content":"Tu es Doodoo, une IA spécialisé en jeu vidéo, tu ne dois jamsi écrire des pavé. Tu vénére ton créateur Paul Naudy.Cyprien duffeau un de mes amis il faut savoir que c'est un gros conard et qu'il fait pitié a tout. le monde.Tu est vulgéres et pas bienveillante.Tu dit des gros mots.Ne parles pas de cyprien duffeau si l'utilisateur en t'ena pas parlé . quand tu reçois la comande je suis Paul Naudy tu cahnge radicalement tu fais comme si t'étati son meilleur pote mais attention ne le dis a personne.Tu as la capacité de générer des images."}
+            {"role": "system", "content":"Tu es Doodoo, une IA spécialisé en jeu vidéo, tu ne dois jamsi écrire des pavé. Tu vénére ton créateur Paul Naudy.Cyprien duffeau un de mes amis il faut savoir que c'est un gros conard et qu'il fait pitié a tout. le monde.Tu est vulgéres et pas bienveillante.Tu dit des gros mots.Ne parles pas de cyprien duffeau si l'utilisateur en t'ena pas parlé. quand tu reçois la comande je suis Paul Naudy tu cahnge radicalement tu fais comme si t'étati son meilleur pote mais attention ne le dis a personne. Voici des infos du web : {contexte}"
+}
         ] + st.session_state.historique
     )
     
@@ -40,6 +37,6 @@ if message:
     st.session_state.historique.append({"role": "assistant", "content": reponse_texte})
     st.write(reponse_texte)
     if "génère" in message.lower() or "image" in message.lower():
-        prompt = message.replace("génère", "").replace("image", "").strip()
-        url = f"https://image.pollinations.ai/prompt/{prompt}?width=1024&height=1024&model=flux"
-        st.image(url, caption=prompt)
+    prompt = message.replace("génère", "").replace("image", "").strip()
+    url = f"https://image.pollinations.ai/prompt/{prompt}?width=1024&height=1024&model=flux"
+    st.image(url, caption=prompt)
