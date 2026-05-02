c3447ba59625256ec22223c3024f0cfd5ac98ba8
ParzivalDu31 <paulasarock@gmail.com>
2026-05-02 15:33:50 +0200

    Add files via upload

---
+++ DOODOO.py
@@ -0,0 +1,37 @@
+from groq import Groq
+import streamlit as st
+st.title("Bonjour je suis doodoo l'ia de Paul")
+if "historique" not in st.session_state:
+    st.session_state.historique = []
+st.markdown("""
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Bungee&display=swap');
+    
+    * {
+        font-family: 'Bungee';
+        color: white;
+    }
+    
+    .stApp {
+        background-color: #1a1a2e;
+    }
+</style>
+""", unsafe_allow_html=True)
+client = Groq(api_key="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")
+for msg in st.session_state.historique:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+message = st.chat_input("votre msg...")
+if message:
+    st.session_state.historique.append({"role": "user", "content": message})
+    
+    reponse = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "system", "content":"Tu es Doodoo, une IA spécialisé en jeu vidéo, tu ne dois jamsi écrire des pavé. Tu vénére ton créateur Paul Naudy.Cyprien duffeau un de mes amis il faut savoir que c'est un gros conard et qu'il fait pitié a tout. le monde.Tu est vulgéres et pas bienveillante.Tu dit des gros mots.Ne parles pas de cyprien duffeau si l'utilisateur en t'ena pas parlé . quand tu reçois la comande je suis Paul Naudy tu cahnge radicalement tu fais comme si t'étati son meilleur pote mais attention ne le dis a personne."}
+        ] + st.session_state.historique
+    )
+    
+    reponse_texte = reponse.choices[0].message.content
+    st.session_state.historique.append({"role": "assistant", "content": reponse_texte})
+    st.write(reponse_texte)
